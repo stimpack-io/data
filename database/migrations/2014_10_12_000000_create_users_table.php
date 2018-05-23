ea7edebc5a13fdbe818085e7cf863771679fa2aa
Brainmaniac <olof.jondelius@gmail.com>
2018-05-23 09:53:31 +0200

    remove user.name from db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('nickname');
             $table->string('provider');
             $table->string('provider_id');
