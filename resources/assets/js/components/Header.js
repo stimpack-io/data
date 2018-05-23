6b9ce7377b3dae7a3419e72f7b66b5b0e5792e3d
Brainmaniac <olof.jondelius@gmail.com>
2018-05-23 12:53:52 +0200

    initial commit data front-end fix

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Header extends Component {
-    render() {        
+    render() {
         return (
-            <div id="header" className="header">            
-                <span className="heading-icon-and-app-name">                    
-                    <span className="header-text">data.stimpack.io</span>
-                </span>                
+            <div id="header" className="header">
+                <div className="logo">
+                    <span className="logo-text headline-text-style">Stimpack</span>
+                </div>
+                <div className="menu">
+                    <div className="menu-bottom">
+                    </div>
+                </div>
             </div>
         );
-    }    
+    }
 }
