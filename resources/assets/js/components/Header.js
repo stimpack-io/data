5b1a1cc39f947ff38bfe4a871ba96f3b11940dd0
Brainmaniac <olof.jondelius@gmail.com>
2018-05-26 12:38:51 +0200

    header controlbar works

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -22,22 +22,32 @@ export class Header extends Component {
     renderUserNavigation() {
         if(user) {
             return (
-                <div>
-
-                    <span name="Packs" data-page="Packs" onClick={this.navigate.bind(this)} className="control-bar-item">
+                <span id="controlBar" className="controlBar">
+                    <span data-page="Packs" onClick={this.navigate.bind(this)} className="control-bar-item">
                         <i title="Packs" className={"fas fa-cubes icon-control-bar"}></i><span className="control-bar-item-text">Packs</span>
                     </span>
-                    <p data-page="Profile" onClick={this.navigate.bind(this)}>Profile</p>
-                    <p data-page="Logout" onClick={this.logout.bind(this)}>Logout</p>
-                </div>
+                    <span data-page="Profile" onClick={this.navigate.bind(this)} className="control-bar-item">
+                        <i title="Profile" className={"fas fa-user-circle icon-control-bar"}></i><span className="control-bar-item-text">Profile</span>
+                    </span>
+                    <span data-page="Logout" onClick={this.logout.bind(this)} className="control-bar-item">
+                        <i title="Logout" className={"fas fa-sign-out-alt icon-control-bar"}></i><span className="control-bar-item-text">Logout</span>
+                    </span>
+                </span>
             )
         } else {
             return (
-                <div>
-                    <p data-page="Packs" onClick={this.navigate.bind(this)}>Packs</p>
-                    <p data-page="Login" onClick={this.login.bind(this)}>Login</p>
-                    <p data-page="Register" onClick={this.login.bind(this)}>Register</p>
-                </div>
+                <span id="controlBar" className="controlBar">
+                    <span data-page="Packs" onClick={this.navigate.bind(this)} className="control-bar-item">
+                        <i title="Packs" className={"fas fa-cubes fa-2x icon-control-bar"}></i><span className="control-bar-item-text">Packs</span>
+                    </span>
+                    <span data-page="Login" onClick={this.login.bind(this)} className="control-bar-item">
+                        <i title="Login" className={"fas fa-sign-in-alt icon-control-bar"}></i><span className="control-bar-item-text">Login</span>
+                    </span>
+                    <span data-page="Register" onClick={this.login.bind(this)} className="control-bar-item">
+                        <i title="Register" className={"fas fa-clipboard-list icon-control-bar"}></i><span className="control-bar-item-text">Register</span>
+                    </span>
+
+                </span>
 
             )
 
